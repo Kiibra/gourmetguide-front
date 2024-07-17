18e13396a55ca853712ade1df6904ad514765013
kiibra <kibra99@gmail.com>
2024-07-17 15:58:48 -0400

    initial commit

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './Landing.module.css'
+
+const Landing = () => {
+  return (
+    <main className={styles.container}>
+      <h1>hello, friend </h1>
+    </main>
+  )
+}
+
+export default Landing
